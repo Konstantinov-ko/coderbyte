e21eb343ab128942bfcfc146c25889b4da17c774
Konstantin Konstantinov <k.konstantinov@okko.tv>
2022-03-23 18:22:15 +0300

    The solution of the task ArrayChallenge.

---
+++ src/com/coderbyte/arrayChallenge/arrayChallenge.kt
@@ -0,0 +1,43 @@
+package com.coderbyte.arrayChallenge
+
+/**
+ * Array Challenge
+Have the function ArrayChallenge(arr) take the array of numbers stored in arr which will contain
+integers that represent the amount in dollars that a single stock is worth, and return the maximum
+profit that could have been made by buying stock on day x and selling stock on day y where y > x.
+For example: if arr is [44, 30, 24, 32, 35, 30, 40, 38, 15] then your program should return 16 because at index
+2 the stock was worth $24 and at index 6 the stock was then worth $40, so if you bought the stock at 24
+and sold it at 40, you would have made a profit of $16, which is the maximum profit that could have been made with
+this list of stock prices.
+
+If there is not profit that could have been made with the stock prices, then your program should return -1.
+For example: arr is [10, 9, 8, 2] then your program should return -1.
+Examples
+Input: arrayOf(10,12,4,5,9)
+Output: 5
+Input: arrayOf(14,20,4,12,5,11)
+Output: 8
+ */
+fun main() {
+    println(ArrayChallenge(intArrayOf(44, 30, 24, 32, 35, 30, 40, 38, 15)))
+    println(ArrayChallenge(intArrayOf(10, 12, 4, 5, 9)))
+    println(ArrayChallenge(intArrayOf(14, 20, 4, 12, 5, 11)))
+}
+
+fun ArrayChallenge(arr: IntArray): String {
+    var profit = -1
+    var buy = Int.MAX_VALUE
+
+    for (i in 0 until arr.lastIndex) {
+        val sell = arr[i + 1]
+
+        if (buy > arr[i] && arr[i] < sell) {
+            buy = arr[i]
+        }
+        val currentProfit = sell - buy
+        if (currentProfit > profit) {
+            profit = currentProfit
+        }
+    }
+    return profit.toString()
+}
